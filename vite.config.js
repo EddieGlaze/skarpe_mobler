f497891ae9270a722dbb7878237a1ebb66358cb1
Hanna Waage Hjelmeland <hannahjelmeland@gmail.com>
2025-07-23 12:00:33 +0200

    Update vite config for digital ocean

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/skarpe_mobler/', // EXACTLY this!
+  base: '/', // ← Use root for production on custom domains or DigitalOcean
   plugins: [react()],
-});
+})
