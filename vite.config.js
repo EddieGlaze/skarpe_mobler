453460803d15adb30310f8b6079d7c703d8f8516
EddieGlaze <162360123+EddieGlaze@users.noreply.github.com>
2026-05-16 18:15:25 +0200

    remove olde Vite setup

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // ← Use root for production on custom domains or DigitalOcean
-  plugins: [react()],
-})
